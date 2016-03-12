210e9547be8db4b7a6814aaebb5a9414740c2a31
fortunephasha <u15282920@tuks.co.za>
2016-03-12 13:14:51 +0200

    ims component upload
    Uploading im code

---
+++ StockManagement/ims/ims/DecimalHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ims
+{
+    public static class DecimalHelper
+    {
+        public static string ToHexString(this Decimal dec)
+        {
+            var sb = new StringBuilder();
+            while (dec > 0)
+            {
+                var r = dec % 16;
+                dec /= 16;
+                sb.Insert(0, ((int)r).ToString("X"));
+            }
+            while (sb.ToString().StartsWith("0"))
+            {
+                sb.Remove(0,1);
+            }
+            return sb.ToString();
+        }
+    }
+}
